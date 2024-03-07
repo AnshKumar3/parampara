b64d974ec4aedfa7814a6fcdb2f5adf8a85a5960
unknown <vaibhavchaurasia768@gmail.com>
2024-03-07 15:21:02 -0800

    for parampara

--- src/components/Topbar/Topbar.tsx
+++ src/components/Topbar/Topbar.tsx
@@ -58,7 +58,7 @@ const Topbar: React.FC<TopbarProps> = ({ problemPage }) => {
 				<div className='flex items-center space-x-4 flex-1 justify-end'>
 					<div>
 						<a
-							href='https://www.buymeacoffee.com/burakorkmezz'
+							
 							target='_blank'
 							rel='noreferrer'
 							className='bg-dark-fill-3 py-1.5 px-3 cursor-pointer rounded text-brand-orange hover:bg-dark-fill-2'
