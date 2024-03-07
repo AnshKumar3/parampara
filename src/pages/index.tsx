120cf39a8a2d24f3684828c5550e93ffcc179e05
unknown <vaibhavchaurasia768@gmail.com>
2024-03-07 15:44:29 -0800

    for parampara

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,14 @@
 import ProblemsTable from "@/components/ProblemsTable/ProblemsTable";
 import Topbar from "@/components/Topbar/Topbar";
-
+import useHasMounted from "@/hooks/useHasMounted";
 
 import { useState } from "react";
 
 export default function Home() {
 	const [loadingProblems, setLoadingProblems] = useState(true);
-	
+	const hasMounted = useHasMounted();
 
-
+	if (!hasMounted) return null;
 
 	return (
 		<>
@@ -18,7 +18,7 @@ export default function Home() {
 					className='text-2xl text-center text-gray-700 dark:text-gray-400 font-medium
 					uppercase mt-10 mb-5'
 				>
-					&ldquo; QUALITY OVER QUANTITY &rdquo; 👇
+					&ldquo; CODE DEBUGGING &rdquo; 👇
 				</h1>
 				<div className='relative overflow-x-auto mx-auto px-6 pb-10'>
 					{loadingProblems && (
